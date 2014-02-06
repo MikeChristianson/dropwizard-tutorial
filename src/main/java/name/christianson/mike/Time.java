de36da087b11bb47bf1f184afca4e9d0276417ad
Mike Christianson <mike@codeaweso.me>
2014-02-06 16:16:35 -0700

    initial service output class

---
+++ src/main/java/name/christianson/mike/Time.java
@@ -0,0 +1,11 @@
+package name.christianson.mike;
+
+public class Time {
+    private final String time;
+
+    public Time(String time) {
+        this.time = time;
+    }
+
+    public String getTime() { return time; }
+}
