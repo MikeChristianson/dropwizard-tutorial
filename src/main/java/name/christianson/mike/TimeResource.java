0e478082f98236e4d717f7458ac95da4e082e971
Mike Christianson <mike@codeaweso.me>
2014-02-06 16:23:49 -0700

    initial time resource class

---
+++ src/main/java/name/christianson/mike/TimeResource.java
@@ -0,0 +1,30 @@
+package name.christianson.mike;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Path("/time")
+@Produces(MediaType.APPLICATION_JSON)
+public class TimeResource {
+    private final String defaultTimezone;
+
+    public TimeResource(String defaultTimezone) {
+        this.defaultTimezone = defaultTimezone;
+    }
+
+    @GET
+    public Time getTime(@QueryParam("timezone") String timezone) {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        TimeZone timeZone = TimeZone.getTimeZone(timezone);
+        formatter.setTimeZone(timeZone);
+        String formatted = formatter.format(new Date());
+        return new Time(formatted);
+    }
+}
